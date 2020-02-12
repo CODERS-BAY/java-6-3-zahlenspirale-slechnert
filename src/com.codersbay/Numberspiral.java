bee3f04d7563fd7b9b1c177a00663ef7fb5643c3
Simon <s.lechner.biz@gmail.com>
2020-02-12 15:02:32 +0100

    Basically nothing, WiP

---
+++ src/com.codersbay/Numberspiral.java
@@ -0,0 +1,13 @@
+package com.codersbay;
+
+import java.util.Scanner;
+
+public class Numberspiral {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int startNR = sc.nextInt();
+        System.out.printf( % 3d % 3d % 3d, );
+    }
+
+}
